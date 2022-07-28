53d4e3ef2dc3603a98dce4f0db3d58d8e4264eba
martindumais <dumais21@yahoo.fr>
2022-07-28 07:04:06 +0200

    Utilisation de BufferedReader et Scanner
    Utilisation des classes BufferedReader et Scanner pour permettre à l'utilisateur de saisir le chiffre du mois à afficher

--- Calendar.java
+++ Calendar.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+//import java.util.StringBuilder;
+
 public class Calendar{
-public static void main(String[]args){
-  String c="\nJe suis un calendrier";
+public static void main(String[]args)throws Exception {
+  String c="\n Je suis un Calendrier";
   String c1="inachevé";
   String c2= "\n Au  revoir          Utilisateur  !  ";
-System.out.println(c);
-CalendarMonth jan=new CalendarMonth(4);
+System.out.println("Entrer le chiffre du mois que voulez afficher ?");
+
+BufferedReader myReader=new BufferedReader(new InputStreamReader(System.in));
+String choice= myReader.readLine();
+System.out.println("Vous avez choisi : ".concat(choice));
+System.out.println("Retaper votre choix");
+Scanner myScanner=new Scanner(System.in);
+int choix=myScanner.nextInt();
+System.out.println("Vous avez bien choisi : "+(choix));
+CalendarMonth jan=new CalendarMonth(choix);
 System.out.println(("\nAttention:").concat(c.replace("un calendrier","").trim()).concat(" ").concat(c1.toUpperCase()).concat("."));
 System.out.println(("\nAttention:").concat(c.replace("un calendrier","").trim()).concat(" ").concat(c1).concat(".").length()+" caractères dans la chaine.");
 System.out.println(c2.toUpperCase());System.out.println(c2.toLowerCase());System.out.println(c2.trim());
